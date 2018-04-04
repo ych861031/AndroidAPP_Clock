75879070008b518f29c0cb043fb5ad33fa54b9c8
ych861031 <danny861031@gmail.com>
2018-04-04 23:28:56 +0800

    add textview and set clock

--- app/src/main/java/com/example/yangchunghsuan/clock/MainActivity.java
+++ app/src/main/java/com/example/yangchunghsuan/clock/MainActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editText_minute;
     PendingIntent pendingIntent;
     AlarmManager alarmManager;
+    TextView textView_setview;
 
 
     @Override
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         editText_hour = findViewById(R.id.editTextHour);
         editText_minute = findViewById(R.id.editTextMinute);
+        textView_setview = findViewById(R.id.textView_settime);
 
         Intent intent = new Intent();
         intent.setClass(MainActivity.this,Clock.class);
@@ -32,15 +37,25 @@ public class MainActivity extends AppCompatActivity {
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-
-
     }
 
 
     public void fn_setClock(View v){
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(System.currentTimeMillis());
+
+        c.set(Calendar.HOUR_OF_DAY, Integer.parseInt(editText_hour.getText().toString()));
+        c.set(Calendar.MINUTE, Integer.parseInt(editText_minute.getText().toString()));
+        c.set(Calendar.SECOND,0);
+        c.set(Calendar.MILLISECOND,0);
 
 
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP,c.getTimeInMillis()-500,pendingIntent);
 
         Toast.makeText(MainActivity.this,"鬧鐘設定完成",Toast.LENGTH_SHORT).show();
+
+
+        textView_setview.setText("鬧鐘設定時間:"+editText_hour.getText().toString()+":"+editText_minute.getText().toString());
     }
 }
